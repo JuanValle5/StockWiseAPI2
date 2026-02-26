fa9038910d8b1c03f1c6752dfb72d7b61bf59166
Juan Jose <juan.j.valencia@correounivalle.edu.co>
2026-02-26 10:35:07 -0500

    Initial commit: Proyecto Spring Boot - StockWiseAPI

---
+++ src/main/java/com/JuanValencia/StockWiseAPI/StockWiseApiApplication.java
@@ -0,0 +1,13 @@
+package com.JuanValencia.StockWiseAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockWiseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockWiseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JuanValencia/StockWiseAPI/StockWiseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JuanValencia.StockWiseAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockWiseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
